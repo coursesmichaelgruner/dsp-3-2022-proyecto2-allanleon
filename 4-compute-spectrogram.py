c9ca5c7195e60291c7e2b046a55e060bd3f6272d
Luis Leon <lleon95@gmail.com>
2022-11-27 17:13:15 -0600

    Add spectrogram computation

--- 4-compute-spectrogram.py
+++ 4-compute-spectrogram.py
@@ -4,6 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import librosa
 import librosa.display
+import argparse
+import os
+from mpi4py import MPI
+
+# MPI specific
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+world_size = comm.Get_size()
 
 def compute_spectrogram(audiodata, fs=16000, coldur=0.025, coldist=0.01):
     winlen = int(fs * coldur)
@@ -31,8 +39,77 @@ def plot_spectrogram(S_dB, fs=16000, fmax=8000):
     ax.set(title='Mel-frequency spectrogram')
     plt.show()
 
-if __name__ == "__main__":
-    y, fs = librosa.load('./data/training/down/0a7c2a8d_nohash_0.wav')
+# For a single job
+def single_job(input_file):
+    y, fs = librosa.load(input_file)
     S_dB = compute_spectrogram(y, fs)
     print(S_dB.shape)
     plot_spectrogram(S_dB, fs)
+
+# For MPI task farming
+def process_files(files):
+    total = len(files)
+    total_10perc = total / 10 
+
+    for i in range(total):
+        # Progress tracker
+        if i % 500 == 0:
+            print(f"{rank} has completed {i}/{total} files", flush=1)
+        
+        # Process file
+        file = files[i]
+        y, fs = librosa.load(file)
+        S_dB = compute_spectrogram(y, fs)
+
+        # Save file
+        filename = os.path.splitext(os.path.basename(file))[0]
+        filename = os.path.join(os.path.dirname(file), filename + '.npy')
+        np.save(filename, S_dB)
+        
+
+def open_file(file):
+    f = open(file, 'r')
+    lines = f.readlines()
+    return lines
+
+def parse_lists(path):
+    # Open both files
+    val = os.path.join(path, 'validation_list.txt')
+    train = os.path.join(path, 'training_list.txt')
+    files = open_file(val) + open_file(train)
+    return [os.path.join(path, i.strip('\n')) for i in files]
+
+# Main process
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class = argparse.RawDescriptionHelpFormatter,
+                                     description="Program to compute the spectrograms")
+
+    parser.add_argument('input', help="filename or path to process")
+    parser.add_argument('-s', '--single', action='store_true',
+                        help="If not single, training_list.txt and validation_list.txt" +
+                        " will be opened to process in task-farming mode")
+
+    args = parser.parse_args()
+
+    # Single job process
+    if args.single and rank != 0:
+        exit()
+
+    if args.single and rank == 0:
+        single_job(args.input)
+
+    # Multi job
+    if rank == 0:
+        # Master process
+        print("Reading files from Validation and Training lists ", flush=1)
+        wholelist = parse_lists(args.input)
+        print(f"Total files: {len(wholelist)}", flush=1)
+
+        chunks = np.split(np.array(wholelist), world_size)
+        files = comm.scatter(chunks, root=0)
+    else:
+        # Slave processes
+        files = comm.scatter(None, root=0)
+    
+    print(f"Received {len(files)} in process {rank}")
+    process_files(files)
--- README.md
+++ README.md
@@ -3,8 +3,34 @@
 
 Ejecutar
 
-`
+```bash
 ./1-init.sh
 ./2-classify.sh
 ./3-split-dataset.sh
-`
+mpirun -np 5 --use-hwthread-cpus python3 4-compute-spectrogram.py data/ 2> err.log
+```
+
+For single view:
+
+```bash
+python3 4-compute-spectrogram.py -s data/training/up/9785931e_nohash_0.wav
+```
+
+The `-np` must be multiple of 5
+
+Install
+
+
+MPI for task farming:
+
+```bash
+sudo apt install mpich libopenmpi-dev -y
+```
+
+Python modules for spectrogram:
+
+```bash
+pip3 install librosa numpy matplotlib
+pip3 mpi4py
+```
+
