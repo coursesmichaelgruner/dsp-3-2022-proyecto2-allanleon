63f95e2df36d0d587775646911ce30dfd90e3e01
Allan Navarro <anavarro3106@gmail.com>
2022-11-29 20:05:33 -0600

    normalize array

--- train.py
+++ train.py
@@ -10,40 +10,51 @@ from pathlib import Path
 import os
 import re
 
+rows = 40
+cols = 100
+
 if len(sys.argv) != 2:
     print('usage 7a-normailze.py ./path_to_root_of_data')
     sys.exit(1)
 
-path=sys.argv[1]
+path = sys.argv[1]
 
 labels = ['down', 'go', 'left', 'no', 'off',
-          'on', 'right', 'stop', 'unknown', 'up', 'yes','background_noise']
+          'on', 'right', 'stop', 'unknown', 'up', 'yes', 'background_noise']
 
-training_list_f = open(path+'/training_list.txt','r')
+training_list_f = open(path+'/training_list.txt', 'r')
 training_list = training_list_f.readlines()
 training_list_f.close()
 training_list = [f'{path}/{x}'.strip() for x in training_list]
 
-validation_list_f = open(path+'/validation_list.txt','r')
+validation_list_f = open(path+'/validation_list.txt', 'r')
 validation_list = validation_list_f.readlines()
 validation_list_f.close()
 validation_list = [f'{path}/{x}'.strip() for x in validation_list]
 
-def get_label(file:str):
+
+def get_label(file: str):
     for i in range(len(labels)):
-        label=labels[i]
-        if re.search(f'/{label}/.*.npy',file) is not None:
+        label = labels[i]
+        if re.search(f'/{label}/.*.npy', file) is not None:
             return i
 
 
-def get_array(file_list:list[str],rows=40,cols=100):
-    arr = np.empty((len(file_list),rows,cols))
-    labels = np.empty(len(file_list),dtype=np.uint8)
+def normalize(data):
+    mean = np.mean(data)
+    var = np.var(data)
+    data = (data - mean)/np.sqrt(var)
+    return data
+
+
+def get_array(file_list: list[str], rows=40, cols=100):
+    arr = np.empty((len(file_list), rows, cols))
+    labels = np.empty(len(file_list), dtype=np.uint8)
     for i in range(len(file_list)):
         file = file_list[i]
         if not file:
             continue
-        file = file.replace('.wav','.npy')
+        file = file.replace('.wav', '.npy')
 
         if not os.path.isfile(file):
             print(f'{file} not found')
@@ -51,22 +62,25 @@ def get_array(file_list:list[str],rows=40,cols=100):
         label = get_label(file)
 
         data = np.load(file)
-        r,c = data.shape
+        r, c = data.shape
         if r != rows or c != cols:
-            data=np.pad(data,[(0,rows-r),(0,cols-c)],mode='constant',constant_values=0)
+            data = np.pad(data, [(0, rows-r), (0, cols-c)],
+                          mode='constant', constant_values=0)
 
-        arr[i] = data
+        arr[i] = normalize(data)
         labels[i] = label
-    return (arr,labels)
+    return (arr, labels)
+
 
 print('preparing data')
-training_x,labels_train=get_array(training_list)
-validatioin_x,labels_valid=get_array(validation_list)
+training_x, labels_train = get_array(training_list, rows, cols)
+# validation_x,labels_valid=get_array(validation_list,rows,cols)
+print('preparing data done')
 
 
 data_format = "channels_last"
 
-inp_shape = (40, 100, 1)
+inp_shape = (rows, cols, 1)
 model = keras.models.Sequential()
 
 # 2. Convolution2d
