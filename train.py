a3ec21fba7901fc22198e4aec9cf738fe8cb5793
Allan Navarro <anavarro3106@gmail.com>
2022-11-29 19:52:36 -0600

    Organize data for training

--- train.py
+++ train.py
@@ -4,6 +4,65 @@ import tensorflow as tf
 
 from tensorflow import keras
 import matplotlib.pyplot as plt
+import numpy as np
+import sys
+from pathlib import Path
+import os
+import re
+
+if len(sys.argv) != 2:
+    print('usage 7a-normailze.py ./path_to_root_of_data')
+    sys.exit(1)
+
+path=sys.argv[1]
+
+labels = ['down', 'go', 'left', 'no', 'off',
+          'on', 'right', 'stop', 'unknown', 'up', 'yes','background_noise']
+
+training_list_f = open(path+'/training_list.txt','r')
+training_list = training_list_f.readlines()
+training_list_f.close()
+training_list = [f'{path}/{x}'.strip() for x in training_list]
+
+validation_list_f = open(path+'/validation_list.txt','r')
+validation_list = validation_list_f.readlines()
+validation_list_f.close()
+validation_list = [f'{path}/{x}'.strip() for x in validation_list]
+
+def get_label(file:str):
+    for i in range(len(labels)):
+        label=labels[i]
+        if re.search(f'/{label}/.*.npy',file) is not None:
+            return i
+
+
+def get_array(file_list:list[str],rows=40,cols=100):
+    arr = np.empty((len(file_list),rows,cols))
+    labels = np.empty(len(file_list),dtype=np.uint8)
+    for i in range(len(file_list)):
+        file = file_list[i]
+        if not file:
+            continue
+        file = file.replace('.wav','.npy')
+
+        if not os.path.isfile(file):
+            print(f'{file} not found')
+            sys.exit(1)
+        label = get_label(file)
+
+        data = np.load(file)
+        r,c = data.shape
+        if r != rows or c != cols:
+            data=np.pad(data,[(0,rows-r),(0,cols-c)],mode='constant',constant_values=0)
+
+        arr[i] = data
+        labels[i] = label
+    return (arr,labels)
+
+print('preparing data')
+training_x,labels_train=get_array(training_list)
+validatioin_x,labels_valid=get_array(validation_list)
+
 
 data_format = "channels_last"
 
@@ -86,7 +145,7 @@ model.compile(optimizer=keras.optimizers.Adam(learning_rate=3e-4),
               metrics=[keras.metrics.CategoricalAccuracy(),
                        keras.metrics.FalseNegatives()])
 
-model.fit(x=None,y=None,batch_size=128,epochs=25,shuffle=True)
+model.fit(x=None, y=None, batch_size=128, epochs=25, shuffle=True)
 
 model.save('model.h5')
 
