cec54fd6d823f70ad235cdcf1e08c9f4b058af66
Allan Navarro <anavarro3106@gmail.com>
2022-12-03 08:52:20 -0600

    6-python-clip.py: treat noise as any other command
    This will ease the classification, selection of training and validation data and calculation of spectogram

--- 6-python-clip.py
+++ 6-python-clip.py
@@ -26,7 +26,6 @@ def clip_file(audio_file):
     y, sr = librosa.load(audio_file, sr=16000)
     samples = y.shape[0] // sr
     filename = os.path.splitext(os.path.basename(audio_file))[0]
-    filename = os.path.join(os.path.dirname(audio_file), filename)
     filename_list = []
     
     for i in range(samples * 5):
@@ -34,21 +33,18 @@ def clip_file(audio_file):
         si = i * offset
         ei = si + sr
         y_split = y[si:ei]
-        output_filename = os.path.join(f"{filename}_{i}.wav")
+        output_filename = os.path.join('data','background_noise',f"{filename}_{i}.wav")
         sf.write(output_filename, y_split, 16000, 'PCM_16')
         filename_list.append(output_filename + '\n')
     
     return filename_list
 
 if __name__ == "__main__":
-    listname = 'data/noise_list.txt'
-    path = 'data/background_noise'
+    path = 'data/_background_noise_'
+    os.makedirs('data/background_noise', exist_ok=True)
     files = open_list()
     filelist = []
 
     for i in files:
         filename = os.path.join(path, i).strip('\n')
         loc_list = clip_file(filename)
-        filelist = filelist + loc_list
-    
-    write_file(filelist, listname)
